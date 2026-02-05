f560f2106206f8203770a4302f6929c43abf4adc
Rohy <139153698+RohyfuturBillionaire@users.noreply.github.com>
2026-02-05 11:49:40 +0300

    test server back and front

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // Connexion à MongoDB
-mongoose.connect(process.env.MONGO_URI, {
- useNewUrlParser: true,
- useUnifiedTopology: true
-}).then(() => console.log("MongoDB connecté")).catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {}).then(() => console.log("MongoDB connecté")).catch(err => console.log(err));
+
+app.use('/articles', require('./routes/articleRoutes'));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
