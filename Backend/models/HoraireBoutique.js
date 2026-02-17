1d1a685004f0b6d1b94d903b978364b4144d643f
RohyfuturBillionaire <rohyrajoelina@gmail.com>
2026-02-17 16:20:49 +0300

    commit back horaire

--- Backend/models/HoraireBoutique.js
+++ Backend/models/HoraireBoutique.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const HoraireBoutiqueSchema = new mongoose.Schema({
-  horaire_ouverture: { type: Date, required: true },
-  horaire_fermeture: { type: Date, required: true },
+  horaire_ouverture: { type: String, required: true },
+  horaire_fermeture: { type: String, required: true },
   id_boutique: { type: mongoose.Schema.Types.ObjectId, ref: 'Boutique', required: true },
-  label: { type: Date }
+  label: { type: String, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('HoraireBoutique', HoraireBoutiqueSchema);
---
+++ Frontend/src/app/admin/gestion-boutiques/horaire_boutique/services/horaire-boutique.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HoraireBoutiqueService {
+
+  private apiUrl= `${environment.apiUrl}/horaire`;
+
+
+  constructor(private http: HttpClient) {}
+  getHoraire(): Observable<any> {
+    return this.http.get(`${this.apiUrl}`);
+  }
+
+  addHoraire(horaire: any): Observable<any> {
+    return this.http.post(this.apiUrl, horaire);
+ }
+
+ updateHoraire(id: string, horaire: any): Observable<any> {
+ return this.http.put(`${this.apiUrl}/${id}`, horaire);
+ }
+
+ deleteHoraire(id: string): Observable<any> {
+ return this.http.delete(`${this.apiUrl}/${id}`);
+ }
+}
