67f504d00ef6d780d8e73b30e901a5926f3a3024
RohyfuturBillionaire <rohyrajoelina@gmail.com>
2026-02-26 20:20:12 +0300

    change back

--- Frontend/src/environments/environments.ts
+++ Frontend/src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl:'https://m1p13mean-mamy-rohy.vercel.app',
+    apiUrl:'https://m1p13mean-mamy-rohy-39bluqy29-rohyfuturbillionaires-projects.vercel.app',
 };
